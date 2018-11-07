5db13e7093092875182f153da8997f4b7395c332
nemotea0612@gmail.com <nemotea0612@gmail.com>
2018-11-07 23:43:04 +0900

    body change
    add question

--- my-project/src/components/errorCheckBody.vue
+++ my-project/src/components/errorCheckBody.vue
@@ -4,9 +4,24 @@
   I have a few questions.<br>
   If you all clear, you may be happy !
   <br><br>
-  Question.1<br>
-  Question.2<br>
-  Question.3
+  Question.1 tell me how you are like.
+  <p>
+    <label>Age</label>
+    <input v-model="age">years old
+  </p>
+
+  <p>
+    <label>Sex</label>
+    <input type="radio" name ="sex" value="male" v-model="sex">
+    <label for="male">Male</label>
+    <input type="radio" name ="sex" value="female" v-model="sex">
+    <label for="female">Female</label>
+  </p>
+
+  <p>
+    <label>Favorite food</label>
+    <input v-model="favoriteFood" type="text">
+  </p>
 </div>
 </template>
 
